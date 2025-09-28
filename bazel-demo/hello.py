9e3718f242cd6cabb2ba0a66254b99d3d13f6460
Neha Gade <nehagade715@gmail.com>
2025-09-28 20:00:06 +0530

    minimal

---
+++ bazel-demo/hello.py
@@ -0,0 +1,9 @@
+# hello.py
+
+
+def say():
+ return "Hello from Neha's Bazel!"
+
+
+if __name__ == "__main__":
+ print(say())
---
+++ bazel-demo/test_hello.py
@@ -0,0 +1,8 @@
+# test_hello.py
+
+
+import hello
+
+
+def test_say():
+    assert hello.say() == "Hello from Bazel!"
